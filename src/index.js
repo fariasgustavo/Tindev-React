919c44e4fd9bf8fdeee0fd6315cca7a63d4b1434
devgustavofarias@gmail.com <devgustavofarias@gmail.com>
2019-08-13 22:45:36 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './reset.css';
+import './style.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './routes';
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
